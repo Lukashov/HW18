07c86b1cfaa66ae3bb1cc96f51219e5eda9f95e6
Den <lukashov.d.n@gmail.com>
2015-09-08 22:08:09 +0300

    Add class MarkersModel

---
+++ app/src/main/java/com/example/den/hw18/models/MarkersModel.java
@@ -0,0 +1,12 @@
+package com.example.den.hw18.models;
+
+/**
+ * Created by Den on 08.09.15.
+ */
+public class MarkersModel {
+    private String id;
+    private String latitude;
+    private String longitude;
+    private String text;
+    private String filePath;
+}
